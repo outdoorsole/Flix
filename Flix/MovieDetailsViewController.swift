01ebe7cc0663ea1daddabca73641c2bb0e8be9cf
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-03-26 11:37:16 -0700

    Create new View Controller (MovieDetailsViewController):
    - Add segue from MovieCell of MoviesViewController to MovieDetailsViewController

---
+++ Flix/MovieDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix
+//
+//  Created by Maribel Montejano on 3/26/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
